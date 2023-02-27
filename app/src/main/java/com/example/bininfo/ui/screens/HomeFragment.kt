4bc42f601fcd7f1ff66b7fafbbfc4a8749e215e0
simt11 <simt11@yandex.ru>
2023-02-27 17:24:35 +0300

    10)Добавил компактный AppTextWatcher 11)

--- app/src/main/java/com/example/bininfo/ui/screens/HomeFragment.kt
+++ app/src/main/java/com/example/bininfo/ui/screens/HomeFragment.kt
@@ -1,24 +1,25 @@
 package com.example.bininfo.ui.screens
 
-import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.bininfo.R
+import com.example.bininfo.utilits.AppTextWatcher
+import kotlinx.android.synthetic.main.fragment_home.enterCardNumber
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    private lateinit var response: BinViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onStart() {
+        super.onStart()
+/*        enterCardNumber.addTextChangedListener(AppTextWatcher {
+            val string = enterCardNumber.text.toString()
+            if (string > 4) {
+                val value = response.getBinInfo(string.toInt()).binUiStatus
+                Toast.makeText(context, "$value", Toast.LENGTH_SHORT).show()
+            })
+
+        }*/
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
 }
