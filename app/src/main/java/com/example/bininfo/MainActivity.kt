5b14eb4fa7b31ece81cf2e1289d0bcc6932d27ab
simt11 <simt11@yandex.ru>
2023-02-27 16:53:52 +0300

    5) Добавил в ActivityMain контейнер для фрагментов. 6) Создал стартовый фрагмент 7) Установил автопереход на него. 8) Создал Fun.kt для новых функций. 9) Добавил функцию переход во фрагмент для MainActivity

--- app/src/main/java/com/example/bininfo/MainActivity.kt
+++ app/src/main/java/com/example/bininfo/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.bininfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bininfo.databinding.ActivityMainBinding
+import com.example.bininfo.ui.screens.HomeFragment
+import com.example.bininfo.utilits.replaceFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        replaceFragment(HomeFragment(), false)
     }
 }
---
+++ app/src/main/java/com/example/bininfo/utilits/Fun.kt
@@ -0,0 +1,18 @@
+package com.example.bininfo.utilits
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.bininfo.R
+
+fun AppCompatActivity.replaceFragment(fragment: Fragment, addStack:Boolean = true){
+    if (addStack){
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(null)
+            .replace(R.id.dataContainer, fragment)
+            .commit()
+    } else {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.dataContainer, fragment)
+            .commit()
+    }
+}
